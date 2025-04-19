67cb173b460100c299644eb49ba4443a256abde9
Lucas emanoel <lucasemanoel625@gmail.com>
2025-04-18 21:51:28 -0300

    Conexão com o banco de dados e uso do mysql

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import app from './src/app.js'; // Importa o app do arquivo app.js
+import conexao from './infra/conexao.js'
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+conexao.connect((erro) => {
+    if (erro) {
+        console.log('Erro ao conectar ao banco de dados: ', erro);
+    } else {
+        console.log('Conectado ao banco de dados!');
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+}); 
+
+// O código acima importa o app do arquivo app.js e a conexão com o banco de dados do arquivo conexao.js.
+// Em seguida, define a porta do servidor como 3000 e tenta conectar ao banco de dados.
--- src/app.js
+++ src/app.js
@@ -6,10 +6,11 @@ app.use(express.json()); // Middleware para converter o body da requisição em 
 
 //mock de dados
 const cars = [
-   {id: 1, modelo: 'Tiguan', motor: '2.0', ano: 2010, cliente: 'Pedro'},
-    {id: 2, modelo: 'Civic', motor: '1.5', ano: 2018, cliente: 'Maria'},
-    {id: 3, modelo: 'Corolla', motor: '2.0', ano: 2020, cliente: 'João'},
-    {id: 4, modelo: 'Onix', motor: '1.0', ano: 2021, cliente: 'Ana'}
+   {id: 1, modelo: 'Tiguan', motor: '2.0', ano: 2010, cliente: 'Pedro', situacao: 'Aguardando orçamento', contato: 11996678902},
+    {id: 2, modelo: 'Civic', motor: '1.5', ano: 2018, cliente: 'Maria', situacao: 'Em serviço', contato: 11998278901},
+    {id: 3, modelo: 'Corolla', motor: '2.0', ano: 2020, cliente: 'João', situacao: 'Aguardando aprovação do cliente', contato: 11999878501},    
+    {id: 4, modelo: 'Onix', motor: '1.0', ano: 2021, cliente: 'Ana', situacao: 'Pronto', contato: 11999789567},
+    {id: 5, modelo: 'Fusca', motor: '1.3', ano: 1970, cliente: 'Carlos', situacao: 'Aguardando pagamento', contato: 11999678901},
 ];
 
 function searchCarById(id) {
@@ -46,5 +47,10 @@ app.delete('/cars/:id', (req, res) => {
     res.status(200).send(`Carro com id ${req.params.id} retirado da oficina!`);
 });
 
+app.put('/cars/:id', (req, res) => {
+    const index = searchIndexCar(req.params.id);
+    cars[index].situacao = req.body.situacao;
+    res.json(cars)
+});
 
  export default app; // Exporta o app para ser usado no server.js
